92f10bb20c0c1792d57e2741299ddb13e00ea47e
era91k <erra91.ea@gmail.com>
2021-12-15 12:03:41 +0100

    aaaaaaaaaaaaaa

--- src/V_Connexion.java
+++ src/V_Connexion.java
@@ -62,7 +62,7 @@ public class V_Connexion extends JFrame implements ActionListener {
         this.btnValider.setBounds(150,250,100,30);
         
         //Image
-        this.img = new ImageIcon(getClass().getResource("logo.jpg"));
+        //this.img = new ImageIcon(getClass().getResource("logo.jpg"));
         this.image = new JLabel();
         this.image.setIcon(img);
         this.image.setBounds(100,300,200,200);
